85f900a4d6c6ee9a5621f425855e3d3139e2d396
franciszek-szeptycki <franciszek.szeptycki@gmail.com>
2024-04-13 17:12:16 -0400

    fix

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-payment-intent/', views.create_payment, name='make_payment')
+    path('create-payment-intent/', views.create_payment, name='make_payment'),
+    path('save-stripe-info/', views.save_stripe_info, name='save_stripe_info'),
 ]
--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,9 +1,7 @@
-import json
-
 import stripe
 from django.conf import settings
 from rest_framework.decorators import api_view
-from rest_framework.response import Response as jsonify
+from rest_framework.response import Response
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -30,9 +28,28 @@ def create_payment(request):
                 'enabled': True,
             },
         )
-        return jsonify({
+        return Response({
             'clientSecret': intent['client_secret'],
             'intent': intent
         })
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return Response({'error': str(e)})
+
+
+@api_view(['POST'])
+def save_stripe_info(request):
+    data = request.data
+    email = data['email']
+    payment_method_id = data['payment_method_id']
+    extra_msg = ''
+    customer_data = stripe.Customer.list(email=email).data
+
+    if len(customer_data) == 0:
+        customer = stripe.Customer.create(
+            email=email, payment_method=payment_method_id)
+    else:
+        customer = customer_data[0]
+        extra_msg = "Customer already existed."
+    return Response({'message': 'Success', 'data': {
+        'customer_id': customer.id, 'extra_msg': extra_msg}
+    })
